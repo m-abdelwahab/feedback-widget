5bc23e126268b41b61af9ccb7b718327e2fb4a0c
Mahmoud Abdelwahab <hey@mahmoud.codes>
2022-10-25 21:08:44 +0200

    feat: add zod schema validation

--- pages/api/feedback.ts
+++ pages/api/feedback.ts
@@ -1,10 +1,31 @@
 import { postmark } from 'lib/postmark';
 import type { NextApiRequest, NextApiResponse } from 'next';
+import { z } from 'zod';
 
 const handler = async (req: NextApiRequest, res: NextApiResponse) => {
   try {
     if (req.method !== 'POST') {
       return res.status(405).json({ message: 'Method not allowed' });
+    }
+
+    const schema = z.object({
+      message: z
+        .string({
+          required_error: 'Message is required',
+        })
+        .trim()
+        .min(8, 'Must be 8 or more characters long'),
+      email: z
+        .string({
+          required_error: 'Email is required',
+        })
+        .email('Not a valid email'),
+    });
+
+    const validation = schema.safeParse(req.body);
+
+    if (!validation.success) {
+      return res.status(422).json({ message: validation.error.issues });
     }
 
     const { email, message } = req.body;
